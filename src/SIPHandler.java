f69a5a5986bfc1c94d399e0a212d47ef7d9d1f7e
Niklas Johnson <njohnson@kth.se>
2012-04-16 13:39:54 +0200

    Handler fix

--- src/SIPHandler.java
+++ src/SIPHandler.java
@@ -3,25 +3,49 @@ import java.net.DatagramPacket;
 
 public class SIPHandler extends Thread{
 	private DatagramPacket receivePacket;
-	
-	
-	
+
+
+
 	public SIPHandler(DatagramPacket receivePacket){
 		System.out.println("New thread started");
 		this.receivePacket = receivePacket;
 	}
-	
+
 	public void run(){
+
+
+
+		String inputData = new String(receivePacket.getData());
+		//		 System.out.println(sentence);
+
+		SIPRequestHandler handler = new SIPRequestHandler();
+
+		System.out.println(inputData + "\n");
+		
+		// Store the length of the data for later use.
+//		int	lengthOfData = inputData.split("\r\n|\r|\n").length;
+		System.out.println(inputData + ".\n");
+		inputData = inputData.replace("\n\n", "hej");
+//		System.out.println("NY:" + inputData + ".\n");
+		String [] splitInputData = inputData.split("\r\n|\r|\n");
 		
 		
-		
-		 String sentence = new String(receivePacket.getData());
-//		 System.out.println(sentence);
-		 
-		 
+		System.out.println(splitInputData.length);
 		
 		
+		int real_length = 0;
 		
+		for(int i=0; i<splitInputData.length;i++){
+			if(splitInputData[i].equals("") == true){
+				real_length = i;
+				break;
+			}
+		}			
+		for(int i=0; i<=real_length; i++){
+			handler.inputHandler(splitInputData[i]);
+			System.out.println(splitInputData[i]);
+		}
+		handler.outputHandler("");
 	}
 
 }
