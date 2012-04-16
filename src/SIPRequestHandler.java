9721ef9ca9fde9bba1287ab8ebd15521f374510b
Peter Smogner <psmogner@gmail.com>
2012-04-16 13:50:19 +0200

    no message

--- src/SIPRequestHandler.java
+++ src/SIPRequestHandler.java
@@ -2,6 +2,8 @@
 public class SIPRequestHandler {
 
 	private SIPSession currentSIPSession;
+	private String temporaryString, ringingResponse, okMessage;
+	private String[] stringArray;
 
 	public SIPRequestHandler(){
 		currentSIPSession = new SIPSession();
@@ -12,28 +14,94 @@ public class SIPRequestHandler {
 		if(inputString != null){
 
 			if(inputString.startsWith("INVITE")){
-				System.out.println("<< Incoming Invite");
-				
+				currentSIPSession.setStatus("INVITE");
+				stringArray = inputString.split(" ");
+				currentSIPSession.setTo(stringArray[1]);
+				stringArray = null;
 			}
-			else if(inputString.startsWith("")){
-
+			else if(inputString.startsWith("Via:")){
+				currentSIPSession.setVia(inputString);
 			}
-			else if(inputString.startsWith("")){
-
+			else if(inputString.startsWith("From:")){
+				stringArray = inputString.split(" ");
+				temporaryString = stringArray[1];
+				currentSIPSession.setFrom(temporaryString.split(";")[0]);
+				currentSIPSession.setFromTag(temporaryString.split(";")[1]);
+				temporaryString = null;
+				stringArray = null;
 			}
-			else if(inputString.startsWith("")){
-
+			else if(inputString.startsWith("Call-ID:")){
+				currentSIPSession.setCallID(inputString);
 			}
-			else if(inputString.startsWith("")){
-
+			else if(inputString.startsWith("CSeq:")){
+				stringArray = temporaryString.split(" ");
+				temporaryString = stringArray[0] +" "+ stringArray[1];
+				currentSIPSession.setCSeq(temporaryString);
+				currentSIPSession.setCSeqAttribute(stringArray[2]);
+				temporaryString = null;
+				stringArray = null;
+			}
+			else if(inputString.startsWith("Content-Type: ")){
+				temporaryString = inputString.split(" ")[1];
+				currentSIPSession.setContentType(temporaryString);
+				temporaryString = null;
+			}
+			else if(inputString.startsWith("Allow: ")){
+				currentSIPSession.setAllow(inputString);
+			}
+			else if(inputString.startsWith("Contact: ")){
+				currentSIPSession.setContact(inputString);
+			}
+			else if(inputString.startsWith("UserAgent: ")){
+				currentSIPSession.setUserAgent(inputString);
 			}
 		}
 		return "";
 	}
 
-	public String outputHandler(String inputString) {
+	public String outputHandler() {
+		if(currentSIPSession.equals("INVITE")){
+			RingingResponse();
+			return ringingResponse;
+			
+		}
 		return "";
 	}
 
+	public String RingingResponse(){
+		ringingResponse = null;
+		
+		ringingResponse += "STP/2.0 180 Ringing\n";
+		ringingResponse += currentSIPSession.getVia()+"\n";
+		ringingResponse += "From: "+currentSIPSession.getFrom()+";"+currentSIPSession.getFromTag()+"\n";
+		ringingResponse += "To: <"+currentSIPSession.getTo()+">\n";
+		ringingResponse += currentSIPSession.getCallID()+"\n";
+		ringingResponse += currentSIPSession.getCSeq()+"\n";
+		ringingResponse += currentSIPSession.getContact()+"\n";
+		ringingResponse += currentSIPSession.getUserAgent()+"\n";
+		ringingResponse += "Content-Length: 0\n";
+
+		System.out.println("<< RINGING response created = \n"+ ringingResponse);
+		return ringingResponse;
+
+	}
+
+	public String okMessage(){
+		okMessage = null;
+		
+		okMessage += "SIP/2.0 200 OK\n";
+		okMessage += currentSIPSession.getVia()+"\n";
+		okMessage += "From: "+currentSIPSession.getTo()+">\n";
+		okMessage += "To: <"+currentSIPSession.getFrom()+";"+currentSIPSession.getFromTag()+"\n";
+		okMessage += currentSIPSession.getCallID()+"\n";
+		okMessage += currentSIPSession.getCSeq() +" "+currentSIPSession.getCSeqAttribute()+"\n";
+		okMessage += currentSIPSession.getAllow()+"\n";
+		okMessage += "Accept: "+currentSIPSession.getContentType()+"\n";
+		okMessage += currentSIPSession.getUserAgent()+"\n";
+		okMessage += "Content-Length: 0\n";
+
+		System.out.println("<< OK response created = \n"+ okMessage);
+		return okMessage; 
+	}
 
 }
--- src/SIPSession.java
+++ src/SIPSession.java
@@ -1,4 +1,117 @@
 
 public class SIPSession {
 
+	private String status;
+	private String via;
+	private String from;
+	private String fromTag;
+	private String to;
+	private String CallID;
+	private String CSeq;
+	private String Contact;
+	private String ContentType;
+	private String allow;
+	private String userAgent;
+	private String CSeqAttribute;
+
+	public SIPSession(){
+		
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public String getVia() {
+		return via;
+	}
+
+	public void setVia(String via) {
+		this.via = via;
+	}
+
+	public String getFrom() {
+		return from;
+	}
+
+	public void setFrom(String from) {
+		this.from = from;
+	}
+
+	public String getTo() {
+		return to;
+	}
+
+	public void setTo(String to) {
+		this.to = to;
+	}
+
+	public String getCallID() {
+		return CallID;
+	}
+
+	public void setCallID(String callID) {
+		CallID = callID;
+	}
+
+	public String getCSeq() {
+		return CSeq;
+	}
+
+	public void setCSeq(String cSeq) {
+		CSeq = cSeq;
+	}
+
+	public String getContact() {
+		return Contact;
+	}
+
+	public void setContact(String contact) {
+		Contact = contact;
+	}
+
+	public String getContentType() {
+		return ContentType;
+	}
+
+	public void setContentType(String contentType) {
+		ContentType = contentType;
+	}
+
+	public String getFromTag() {
+		return fromTag;
+	}
+
+	public void setFromTag(String fromTag) {
+		this.fromTag = fromTag;
+	}
+
+	public String getAllow() {
+		return allow;
+	}
+
+	public void setAllow(String allow) {
+		this.allow = allow;
+	}
+
+	public String getUserAgent() {
+		return userAgent;
+	}
+
+	public void setUserAgent(String userAgent) {
+		this.userAgent = userAgent;
+	}
+
+	public String getCSeqAttribute() {
+		return CSeqAttribute;
+	}
+
+	public void setCSeqAttribute(String cSeqAttribute) {
+		CSeqAttribute = cSeqAttribute;
+	}
+	
 }
